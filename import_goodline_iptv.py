bd0ad83c7db7935fa773dc513443104efcb1840d
Nickolay Sadovskiy <sns1081@gmail.com>
2019-03-10 16:31:37 +0700

    Implement parsing bool from command line

--- import_goodline_iptv.py
+++ import_goodline_iptv.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser
 from goodline_iptv.importer import do_import
+from argparse import ArgumentParser, ArgumentTypeError
 
 
 if __name__ == '__main__':
+
+    def str2bool(v):
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise ArgumentTypeError('Boolean value expected.')
+
     parser = ArgumentParser()
 
     parser.add_argument('-o', '--out-dir', required=True, help='Output directory')
     parser.add_argument('-e', '--encoding', default='cp1251', help='Source JTV teleguide encoding')
     parser.add_argument('-t', '--timezone', default='+0700', help='Source JTV teleguide timezone')
-    parser.add_argument('-p', '--pretty-xmltv', default=False, help='Prettify output xmltv')
+    parser.add_argument('-p', '--pretty-xmltv', default=False, type=str2bool, nargs='?',
+                        const=True, help='Prettify output xmltv')
     parser.add_argument('-v', '--verbosity', default=0, type=int, choices=range(0, 4), help='Verbosity level')
 
     args = parser.parse_args()
+    print(type(args.pretty_xmltv), args.pretty_xmltv, bool(args.pretty_xmltv))
 
     do_import(args.verbosity, args.out_dir, args.encoding, args.timezone, args.pretty_xmltv)
