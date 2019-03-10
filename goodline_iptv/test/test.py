8e618f25703b5ad6f9a6961ea8ef47f09602bec0
Nickolay Sadovskiy <sns1081@gmail.com>
2019-03-10 13:25:20 +0700

    Fix tests for jtv parser

--- goodline_iptv/test/test.py
+++ goodline_iptv/test/test.py
@@ -1,17 +1,41 @@
+import logging
 import unittest
-from goodline_iptv.jtvfile import parse_pdt, parse_ndx
-from goodline_iptv.test.jtv_data import PDT_DATA, NDX_DATA, PDT_RESULT, NDX_RESULT
+from io import BytesIO
+from datetime import datetime
+from goodline_iptv.jtvfile import JtvFile
+from goodline_iptv.test.jtv_data import TV_ZIP
+
+
+logging.basicConfig(
+    handlers=[
+        logging.NullHandler()
+    ]
+)
+
+log = logging.getLogger()
 
 
 class TestJtv(unittest.TestCase):
 
-    def test_parse_pdt(self):
-        result = parse_pdt(PDT_DATA)
-        self.assertDictEqual(result, PDT_RESULT)
+    def setUp(self):
+        self.channels = list(JtvFile(BytesIO(TV_ZIP), log).channels)
 
-    def test_parse_ndx(self):
-        result = parse_ndx(NDX_DATA)
-        self.assertListEqual(result, NDX_RESULT)
+    def test_channelCount(self):
+        self.assertEqual(len(self.channels), 1)
+
+    def test_channelId(self):
+        self.assertEqual(self.channels[0].track_id, '0')
+
+    def test_tracks(self):
+        tracks = list(self.channels[0].tracks)
+
+        self.assertEqual(len(tracks), 166)
+
+        self.assertEqual(tracks[0][0], datetime(2019, 3, 4, 5, 0))
+        self.assertEqual(tracks[0][1], 'Телеканал "Доброе утро"')
+
+        self.assertEqual(tracks[165][0], datetime(2019, 3, 11, 4, 5))
+        self.assertEqual(tracks[165][1], 'Давай поженимся! (16+)')
 
 
 if __name__ == '__main__':
